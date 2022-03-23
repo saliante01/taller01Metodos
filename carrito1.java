3a540150bb47e39ef0f0ab63c0624511bed308e6
saliante01 <76070484+saliante01@users.noreply.github.com>
2022-03-23 10:17:38 -0300

    Add files via upload

--- carrito1.java
+++ carrito1.java
@@ -1,10 +1,25 @@
 public class carrito1 {
-    public static int randomnum(){
-        int n=0;
-        
+    public static void main(String[] args){
+
     }
     public static int randomarreglo(){
+        int min=0;
+        int max=21;
 
-        int a[]= A= new int [n];
+        Random random = new Random();
+        int n =  random.nextInt(max + min) + min;
+        int [] miArreglo=new int [n];
+
+        for(int i =0;i<16;i++){
+            miArreglo[i]= int (Math.random()*16);
+    }
+
+    public static int productos(){
+
+
+
+        }
+
+
     }
 }
